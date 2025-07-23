488388147908d7661e6affade2421d9cae924799
Eduardo Robles Espinosa <47559667+eduardoroblesespinosa@users.noreply.github.com>
2025-07-23 12:19:13 -0600

    Update events.js

--- data/events.js
+++ data/events.js
@@ -1,6 +1,6 @@
 export const concerts = [
     {
-        artist: "Stellar Echoes",
+        artist: "Fuego Primordial",
         date: "15 de Mayo, 2026",
         time: "20:00",
         venue: "Arena Ciudad de México",
@@ -8,7 +8,7 @@ export const concerts = [
         image: 'https://images.unsplash.com/photo-1540039155733-5bb30b53aa14?w=800'
     },
     {
-        artist: "Crimson Cascade",
+        artist: "Agua Solar",
         date: "22 de Junio, 2026",
         time: "21:00",
         venue: "Arena Ciudad de México",
@@ -16,7 +16,7 @@ export const concerts = [
         image: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?w=800'
     },
     {
-        artist: "Neon Phantoms",
+        artist: "Espejo Cósmico",
         date: "18 de Julio, 2026",
         time: "20:30",
         venue: "Arena Ciudad de México",
@@ -24,7 +24,7 @@ export const concerts = [
         image: 'https://images.unsplash.com/photo-1524368535928-5b5e00ddc76b?w=800'
     },
     {
-        artist: "Solar Bloom",
+        artist: "Raíz de la Tierra",
         date: "05 de Agosto, 2026",
         time: "19:00",
         venue: "Arena Ciudad de México",
@@ -32,7 +32,7 @@ export const concerts = [
         image: 'https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=800'
     },
     {
-        artist: "Lunar Tides",
+        artist: "Ascensión de la Luz",
         date: "10 de Septiembre, 2026",
         time: "21:00",
         venue: "Arena Ciudad de México",
